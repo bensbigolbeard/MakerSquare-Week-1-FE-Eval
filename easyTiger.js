05ceecb54950ddcd616f87aee28ae0c0aee981d3
Ben White <ben.white@utexas.edu>
2013-11-17 17:16:52 -0600

    Atmosphere mostly done. Next is the Food section

--- easyTiger.js
+++ easyTiger.js
@@ -2,9 +2,11 @@ jQuery(document).ready(function($){
 	$(window).scroll(function(event){
 	    var top = $(document).scrollTop();
 	   
-	    $("#nav").toggleClass(" hide", top < 440);
+	    $("#nav").toggleClass(" hide", top < 440 || top > 800);
 
     	$("#body").toggleClass(" hide", top < 200 );
+
+    	// $("#atmos").toggleClass(" fixed", top < 800 );
 
     // $(".two").toggleClass(" hide", top < 1200 || top > 1700);
   });
